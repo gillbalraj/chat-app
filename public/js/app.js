3e2cb009c89e5e10cff4443d553f4e740af052fd
gillbalraj <balrajgill18@gmail.com>
2018-06-05 17:24:26 -0400

    add message to UI

--- public/js/app.js
+++ public/js/app.js
@@ -2,9 +2,29 @@ var socket = io();
  socket.on('connect', function(){
  	console.log('connected to socket.io server');
  });
-
+//call back function has an argument which is the data server has sent
  socket.on('message', function(message){
  	console.log('New message');
  	console.log(message.text);
- })
+ 	 //to select by class name we use periods
+ 	 jQuery('.messages').append('<p>'+ message.text + '</p>') 
+ });
 
+ //send submitted data front-end
+
+ var $form = jQuery('#message-form');
+
+ $form.on('submit', function(event){
+ 	event.preventDefault();  //to take care of refreshing the page
+ 	
+ 	var $message = $form.find('input[name=message]');
+ 
+ 	socket.emit('message', {
+ 		text: $message.val()
+ 		 //selector of input tags using jquery function
+ 	 	//val functionis used to actually pull the value out and print it as a string
+ 	
+ 	})
+
+$message.val(" ");
+ });
