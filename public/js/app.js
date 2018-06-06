046d876433348c36ce82bf42f42cccdc4266d3a9
gillbalraj <balrajgill18@gmail.com>
2018-06-06 15:58:14 -0400

    add timeStamp

--- public/js/app.js
+++ public/js/app.js
@@ -4,10 +4,13 @@ var socket = io();
  });
 //call back function has an argument which is the data server has sent
  socket.on('message', function(message){
+ 	var timeStampMoment = moment.utc(message.timeStamp);
  	console.log('New message');
- 	console.log(message.text);
+ 	console.log(message.time+' '+message.text);
+ 	var timeStampMoment = moment.utc(message.timeStamp);
+ 	
  	 //to select by class name we use periods
- 	 jQuery('.messages').append('<p>'+ message.text + '</p>') 
+ 	 jQuery('.messages').append('<p> <strong>'+timeStampMoment.local().format('hh:mm a')+': '+'</strong>'+ message.text + '</p>') 
  });
 
  //send submitted data front-end
@@ -20,10 +23,10 @@ var socket = io();
  	var $message = $form.find('input[name=message]');
  
  	socket.emit('message', {
- 		text: $message.val()
+ 		text: $message.val(),
+ 		
  		 //selector of input tags using jquery function
  	 	//val functionis used to actually pull the value out and print it as a string
- 	
  	})
 
 $message.val(" ");
--- server.js
+++ server.js
@@ -3,20 +3,22 @@ var express = require('express');
 var app = express(app);
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var moment = require('moment');
 
-
+var now = moment();
 app.use(express.static(__dirname+'/public'));
 
 io.on('connection', function(socket){
-	console.log('user connected via socket.io!')
-
+	console.log(now.format('hh:mm a ') +'user connected via socket.io!');
 	socket.on('message', function(message){
 		console.log('Message received: '+ message.text);
+		message.timeStamp = now.valueOf();
 		io.emit('message',message);
 		//socket.broadcast.emit('message', message);
 	});
 	socket.emit('message', {
-		text: 'Welcome to my app'
+		text: 'Welcome to my app', 
+		timeStamp: now.valueOf()
 	});
 });
 http.listen(PORT, function(){
